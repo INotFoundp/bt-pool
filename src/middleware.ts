1a8a680b556ca8e3a1da65ca908833aae6cf745f
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2025-01-24 23:48:33 +0330

    FIX MIDDLEWARE

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import {NextRequest} from "next/server";
 import {error} from "@/backend/tools/respones";
+import host from "@/server/host";
 
 export async function middleware(req: NextRequest) {
 
@@ -8,8 +9,8 @@ export async function middleware(req: NextRequest) {
     if (paths[2] === "user") {
         const token = req.cookies.get("token")?.value
         try {
-            const user = await fetch(`http://localhost:3000/api/check?token=${token}`).then(r => r.json())
-    
+            const user = await fetch(`${host}/api/check?token=${token}`).then(r => r.json())
+
             if (!user.ok) return error("Unauthorized", 401);
         } catch (e) {
             return error(e + "")
